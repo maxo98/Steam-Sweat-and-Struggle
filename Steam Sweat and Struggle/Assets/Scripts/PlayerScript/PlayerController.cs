76314c3591c5a4d9ec0e3c809109c5d3ce129b95
Grandkhan <darkjade2000@gmail.com>
2020-02-13 19:58:22 +0100

    projectiles dans tt les direction + debut du dash

--- Steam Sweat and Struggle/Assets/Scripts/PlayerScript/PlayerController.cs
+++ Steam Sweat and Struggle/Assets/Scripts/PlayerScript/PlayerController.cs
@@ -5,151 +5,261 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    //speed and jumpSpeed 
-    [SerializeField]
-    private float speed = 30.0f;
-    [SerializeField]
-    private float jumpForce = 110.0f;
-    [SerializeField]
-    private float fallSpeed = -70.0f;
-    [SerializeField]
-    private float offsetProjectile = 5.0f;
+	//speed and jumpSpeed 
+	[SerializeField]
+	private float speed = 30.0f;
+	[SerializeField]
+	private float jumpForce = 110.0f;
+	[SerializeField]
+	private float fallSpeed = -70.0f;
+	[SerializeField]
+	private float dashSpeed = 400f;
 
-    //inputs
-    private float HInput;
-    private bool JInput;
+	//inputs
+	private float HInput;
+	private float VInput;
+	private bool JInput;
+	private float VGazeInput;
+	private float HGazeInput;
 
-    //jump condition
-    private bool isGrounded = false;
-    private float lastTimeGrounded;
-    [SerializeField]
-    private float fallMultiplier = 20.0f;
-    [SerializeField]
-    private float lowJumpMultiplier = 40f;
-    [SerializeField]
-    private float rememberGroundedFor = 0f;
-    [SerializeField]
-    private float fireRate = 1f;
+	//jump condition
+	private bool isGrounded = false;
+	private float lastTimeGrounded;
+	[SerializeField]
+	private float fallMultiplier = 20.0f;
+	[SerializeField]
+	private float lowJumpMultiplier = 40f;
+	[SerializeField]
+	private float rememberGroundedFor = 0f;
 
-    [SerializeField]
-    private float nextFire;
 
-    //hitbox components
-    private Rigidbody2D body;
-    [SerializeField]
-    private Transform groundChecker;
-    [SerializeField]
-    private float groundCheckerRadius = 1f;
-    [SerializeField]
-    private LayerMask groundLayer;
+	//hitbox components
+	private Rigidbody2D body;
+	[SerializeField]
+	private Transform groundChecker;
+	[SerializeField]
+	private float groundCheckerRadius = 1f;
+	[SerializeField]
+	private LayerMask groundLayer;
 
+	//projectile & dash components
 	[SerializeField]
 	private GameObject projectilePrefab;
-	private float look = 1;
+	[SerializeField]
+	private float offsetProjectileX = 5.0f;
+	[SerializeField]
+	private float offsetProjectileY = 6.5f;
+	[SerializeField]
+	private float fireRate = 1f;
+	[SerializeField]
+	private float nextFire;
+	[SerializeField]
+	public float dashRate = 2f;
+	[SerializeField]
+	public float nextDash;
+
+	//where the character is looking
+	private float gazeDirectionAngle;
+	private float gazeDirectionY = 0;
+	private float gazeDirectionX = 1;
 
 
-	private float throwDirectionX = 0;
-	private float throwDirectionY = 0;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        //get the components
-        body = GetComponent<Rigidbody2D>();
-        nextFire = Time.time;
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-		if (HInput < -0.01)
-			look = -1;
-		if (HInput > 0.01)
-			look = 1;
-        Move();
-        Throw();
-        CheckIfGrounded();
-        BetterJump();
-        Jump();
-        
-        
-    }
-
-    //movement methode
-    private void Move()
-    {
-        HInput = Input.GetAxis("HorizontalPlayerOne");
-
-        //moving the character on the X axis
-        body.velocity = new Vector2(HInput * speed, body.velocity.y);
-
-        if (body.velocity.y < fallSpeed)
-        {
-            body.velocity = new Vector2(body.velocity.x,fallSpeed);
-        }
-    }
-
-    //Jump methode
-    private void Jump()
-    {
-        JInput = Input.GetButton("JumpPlayerOne");
-        if (Input.GetButton("JumpPlayerOne") && (isGrounded || Time.time - lastTimeGrounded <= rememberGroundedFor))
-        {
-            body.velocity = new Vector2(body.velocity.x, jumpForce);
-        }
-        
-    }
-
-    private void BetterJump()
-    {
-        if (body.velocity.y > -4 && !Input.GetButton("JumpPlayerOne"))
-        {
-            body.velocity += Vector2.up * Physics2D.gravity * (lowJumpMultiplier - 1) * Time.deltaTime;
-        } else {
-            body.velocity += Vector2.up * Physics2D.gravity * (fallMultiplier - 1) * Time.deltaTime;
-        }
-
-    }
-
-    private void CheckIfGrounded()
-    {
-        Collider2D collider = Physics2D.OverlapCircle(groundChecker.position, groundCheckerRadius, groundLayer);
-
-        if (collider != null)
-        {
-            isGrounded = true;
-        }
-        else
-        {
-            if (isGrounded)
-            {
-                lastTimeGrounded = Time.time;
-            }
-            isGrounded = false;
-        }
-    }
-
-    private void Throw()
+	// Start is called before the first frame update
+	void Start()
 	{
+		//get the components
+		body = GetComponent<Rigidbody2D>();
+		nextFire = Time.time;
+	}
 
-		if (HInput < -0.01)
-			throwDirectionX = -1;
-		if (HInput > 0.01)
-			throwDirectionX = 1;
-        
-		if (Input.GetButton("FirePlayerOne") && Time.time>nextFire)
+	// Update is called once per frame
+	void Update()
+	{
+		Move();
+		Throw();
+		CheckIfGrounded();
+		BetterJump();
+		Jump();
+	}
+
+	//movement method
+	private void Move()
+	{
+		HInput = Input.GetAxis("HorizontalPlayerOne");
+
+		//moving the character on the X axis
+		body.velocity = new Vector2(HInput * speed, body.velocity.y);
+
+		if (body.velocity.y < fallSpeed)
 		{
-			
-            nextFire = Time.time + fireRate;
-            Shoot();
+			body.velocity = new Vector2(body.velocity.x, fallSpeed);
 		}
 	}
-    private void Shoot()
-    {
-        GameObject projectile = Instantiate(projectilePrefab, new Vector3(transform.position.x + throwDirectionX * offsetProjectile, transform.position.y, 0), projectilePrefab.transform.rotation);
-        projectile.GetComponent<Teleportation>().SetMapData(gameObject.GetComponent<Teleportation>().GetMapData());
-        ProjectileMovements scriptProjectile = projectile.GetComponent<ProjectileMovements>();
-        scriptProjectile.setThrowDirection(throwDirectionX);
-        Teleportation scriptTel = projectile.GetComponent<Teleportation>();
-    }
+
+	//Jump method
+	private void Jump()
+	{
+		JInput = Input.GetButton("JumpPlayerOne");
+		if (Input.GetButton("JumpPlayerOne") && (isGrounded || Time.time - lastTimeGrounded <= rememberGroundedFor))
+		{
+			body.velocity = new Vector2(body.velocity.x, jumpForce);
+		}
+
+	}
+
+	private void BetterJump()
+	{
+		if (body.velocity.y > -4 && !Input.GetButton("JumpPlayerOne"))
+		{
+			body.velocity += Vector2.up * Physics2D.gravity * (lowJumpMultiplier - 1) * Time.deltaTime;
+		}
+		else
+		{
+			body.velocity += Vector2.up * Physics2D.gravity * (fallMultiplier - 1) * Time.deltaTime;
+		}
+
+	}
+
+	private void CheckIfGrounded()
+	{
+		Collider2D collider = Physics2D.OverlapCircle(groundChecker.position, groundCheckerRadius, groundLayer);
+
+		if (collider != null)
+		{
+			isGrounded = true;
+		}
+		else
+		{
+			if (isGrounded)
+			{
+				lastTimeGrounded = Time.time;
+			}
+			isGrounded = false;
+		}
+	}
+
+	private void Throw()
+	{
+		//throw inputs : 
+		//vertical
+		float VGazeInput = Input.GetAxis("VerticalGazePlayerOne");
+		//horizontal
+		float HGazeInput = Input.GetAxis("HorizontalGazePlayerOne");
+
+
+		float throwDirectionTmp = gazeDirectionX;
+
+		if (!Input.GetButton("HorizontalGazePlayerOne"))
+			gazeDirectionX = 0;
+		else if (HGazeInput < -0.01)
+			gazeDirectionX = -1;
+		else if (HGazeInput > 0.01)
+			gazeDirectionX = 1;
+
+		if (!Input.GetButton("VerticalGazePlayerOne"))
+			gazeDirectionY = 0;
+		else if (VGazeInput < -0.01)
+			gazeDirectionY = -1;
+		else if (VGazeInput > 0.01)
+			gazeDirectionY = 1;
+
+		if (gazeDirectionX == 0 && gazeDirectionY == 0)
+		{
+			gazeDirectionX = throwDirectionTmp;
+			if (HInput < -0.01)
+				gazeDirectionX = -1;
+			else if (HInput > 0.01)
+				gazeDirectionX = 1;
+		}
+
+		SetGazeAngle();
+
+		if (Input.GetButton("FirePlayerOne") && Time.time > nextFire)
+		{
+			nextFire = Time.time + fireRate;
+			Shoot();
+		}
+
+		if (Input.GetButton("DashPlayerOne") && Time.time > nextDash)
+		{
+			nextDash = Time.time + dashRate;
+			Dash();
+		}
+	}
+
+	private void Shoot()
+	{
+		//instanciate the projectile
+		GameObject projectile = Instantiate(
+						projectilePrefab,
+						new Vector3(transform.position.x + gazeDirectionX * offsetProjectileX, transform.position.y + gazeDirectionY * offsetProjectileY, 0),
+						projectilePrefab.transform.rotation);
+
+		projectile.GetComponent<Teleportation>().SetMapData(gameObject.GetComponent<Teleportation>().GetMapData());
+		ProjectileMovements scriptProjectile = projectile.GetComponent<ProjectileMovements>();
+		scriptProjectile.SetDirectionAngle(gazeDirectionAngle);
+		Teleportation scriptTel = projectile.GetComponent<Teleportation>();
+
+	}
+
+	/** pas super au point **/
+	private void Dash()
+	{
+		body.velocity = (new Vector2(0, 0));
+
+		//we use cos(angle) and sin(angle) to normalize speed in every direction
+		body.AddForce(new Vector2(transform.right.x * dashSpeed * Mathf.Cos(gazeDirectionAngle), transform.up.y * dashSpeed/2 * Mathf.Sin(gazeDirectionAngle)), ForceMode2D.Impulse);
+	}
+
+
+	//sets the character's gaze direction angle in radians
+	public void SetGazeAngle()
+	{
+		switch (gazeDirectionY)
+		{
+			case 0:
+				switch (gazeDirectionX)
+				{
+					case 1:
+						gazeDirectionAngle = 0;
+						break;
+					case -1:
+						gazeDirectionAngle = Mathf.PI;
+						break;
+				}
+				break;
+			case 1:
+				switch (gazeDirectionX)
+				{
+					case 1:
+						gazeDirectionAngle = Mathf.PI / 4;
+						break;
+					case 0:
+						gazeDirectionAngle = Mathf.PI / 2;
+						break;
+					case -1:
+						gazeDirectionAngle = 3 * Mathf.PI / 4;
+						break;
+				}
+				break;
+			case -1:
+				switch (gazeDirectionX)
+				{
+					case 1:
+						gazeDirectionAngle = 7 * Mathf.PI / 4;
+						break;
+					case 0:
+						gazeDirectionAngle = 3 * Mathf.PI / 2;
+						break;
+					case -1:
+						gazeDirectionAngle = 5 * Mathf.PI / 4;
+						break;
+				}
+				break;
+			default:
+				break;
+		}
+	}
 }
