2bbf63217acc71a5280026d6e1ec3df8fbb4ce8e
maxo98 <maxime.boidin.98@gmail.com>
2020-03-05 15:30:44 +0100

    handmade merge from menus into controls

---
+++ Steam Sweat and Struggle/Assets/Scripts/MenusScripts/CharacterSwitcher.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CharacterSwitcher : MonoBehaviour
+{
+    public GameObject leftArrow;
+    public GameObject rightArrow;
+    public GameObject confirm;
+
+    public GameObject character1;
+    public GameObject character2;
+    public GameObject character3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        leftArrow.SetActive(true);
+        rightArrow.SetActive(true);
+        character1.SetActive(true);
+        character2.SetActive(false);
+        character3.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Switcher()
+    {
+        if (character1.activeSelf == true)
+        {
+            character1.SetActive(false);
+            character2.SetActive(true);
+            character3.SetActive(false);
+        }
+        else if (character2.activeSelf == true)
+        {
+            character1.SetActive(false);
+            character2.SetActive(false);
+            character3.SetActive(true);
+        }
+        else if (character3.activeSelf == true)
+        {
+            character1.SetActive(true);
+            character2.SetActive(false);
+            character3.SetActive(false);
+        }
+    }
+
+    public void Confirm()
+    {
+        if (leftArrow.activeSelf == true)
+        {
+            leftArrow.SetActive(false);
+            rightArrow.SetActive(false);
+        }
+        else
+        {
+            leftArrow.SetActive(true);
+            rightArrow.SetActive(true);
+        }
+    }
+
+}
