e65226947626198f380ed1e39271631aa15bd61f
maxo98 <maxime.boidin.98@gmail.com>
2020-03-24 16:25:56 +0100

    controls in menu updated

--- Steam Sweat and Struggle/Assets/Scripts/MenusScripts/CharacterSwitcher.cs
+++ Steam Sweat and Struggle/Assets/Scripts/MenusScripts/CharacterSwitcher.cs
@@ -6,10 +6,7 @@ using UnityEngine;
 
 public class CharacterSwitcher : MonoBehaviour
 {
-    [SerializeField]
-    private int selectionPlayerId;
-    [SerializeField]
-    private GameObject selectionPlayer;
+ 
     [SerializeField]
     private GameObject leftArrow;
     [SerializeField]
@@ -23,91 +20,81 @@ public class CharacterSwitcher : MonoBehaviour
     [SerializeField]
     private GameObject character3;
 
-    private InputManager inputs;
-
     // Start is called before the first frame update
     void Start()
     {
-        selectionPlayerId = Int32.Parse(this.name.Split('r')[1]);
-        inputs = GetComponent<InputManager>();
-        if(!inputs.SetInputs(selectionPlayerId))
-        {
-            this.gameObject.SetActive(false);
-        }
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        Switcher();
-        Confirm();
+
     }
 
-    public void Switcher()
+    void OnSubmit()
     {
-        if (inputs.GetRBPressed())
+        if (leftArrow.activeSelf == true)
         {
-            if (character1.activeSelf == true)
-            {
-                character1.SetActive(false);
-                character2.SetActive(true);
-                character3.SetActive(false);
-            }
-            else if (character2.activeSelf == true)
-            {
-                character1.SetActive(false);
-                character2.SetActive(false);
-                character3.SetActive(true);
-            }
-            else if (character3.activeSelf == true)
-            {
-                character1.SetActive(true);
-                character2.SetActive(false);
-                character3.SetActive(false);
-            }
+            Debug.Log("arrow deactivated");
+            leftArrow.SetActive(false);
+            rightArrow.SetActive(false);
+            confirm.SetActive(false);
         }
-        if(inputs.GetLBPressed())
+        else
         {
-            if (character1.activeSelf == true)
-            {
-                character1.SetActive(false);
-                character2.SetActive(false);
-                character3.SetActive(true);
-            }
-            else if (character2.activeSelf == true)
-            {
-                character1.SetActive(true);
-                character2.SetActive(false);
-                character3.SetActive(false);
-            }
-            else if (character3.activeSelf == true)
-            {
-                character1.SetActive(false);
-                character2.SetActive(true);
-                character3.SetActive(false);
-            }
-        }        
+            Debug.Log("arrow activated");
+            leftArrow.SetActive(true);
+            rightArrow.SetActive(true);
+            confirm.SetActive(true);
+        }
+    }
+    void OnCancel()
+    {
+
     }
 
-    public void Confirm()
+    void OnLeft()
     {
-        if (inputs.GetAPressed() && inputs.GetHorizontalMovement() == 0)
+        if (character1.activeSelf == true)
         {
-            if(leftArrow.activeSelf == true)
-            {
-                Debug.Log("arrow deactivated");
-                leftArrow.SetActive(false);
-                rightArrow.SetActive(false);
-                confirm.SetActive(false);
-            }
-            else
-            {
-                Debug.Log("arrow activated");
-                leftArrow.SetActive(true);
-                rightArrow.SetActive(true);
-                confirm.SetActive(true);
-            }
+            character1.SetActive(false);
+            character2.SetActive(false);
+            character3.SetActive(true);
+        }
+        else if (character2.activeSelf == true)
+        {
+            character1.SetActive(true);
+            character2.SetActive(false);
+            character3.SetActive(false);
+        }
+        else if (character3.activeSelf == true)
+        {
+            character1.SetActive(false);
+            character2.SetActive(true);
+            character3.SetActive(false);
         }
     }
 
+    void OnRight()
+    {
+        if (character1.activeSelf == true)
+        {
+            character1.SetActive(false);
+            character2.SetActive(true);
+            character3.SetActive(false);
+        }
+        else if (character2.activeSelf == true)
+        {
+            character1.SetActive(false);
+            character2.SetActive(false);
+            character3.SetActive(true);
+        }
+        else if (character3.activeSelf == true)
+        {
+            character1.SetActive(true);
+            character2.SetActive(false);
+            character3.SetActive(false);
+        }
+    }
 }
