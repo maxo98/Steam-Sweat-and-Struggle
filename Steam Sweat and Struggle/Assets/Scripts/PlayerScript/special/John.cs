925addc3532851870c6788d7ed346a8659d52b4a
tserrania <tserrania@gmail.com>
2020-04-03 15:41:25 +0200

    Ajout de la préfab pour John

---
+++ Steam Sweat and Struggle/Assets/Scripts/PlayerScript/special/John.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class John : PlayerController
+{
+    private bool hasUsedFreeze = false;
+
+    protected override void InitCharacterSpecs() {
+        
+        NbShots = NbRemainingShots = 1;
+        Reload = 4.0f;
+        Cooldown = 2.0f;
+        projectileSpeed = 200f;
+    }
+
+    protected override void OnSkill() {
+        if (!hasUsedFreeze) {
+            hasUsedFreeze = true;
+        }
+    }
+
+}
